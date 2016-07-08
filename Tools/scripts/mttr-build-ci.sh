e3b71323430a72b9d0a87c7354c9b0a01598ce76
Jonathan Challinger <mr.challinger@gmail.com>
2016-07-08 13:38:33 -0700

    travis test

--- Tools/scripts/mttr-build-ci.sh
+++ Tools/scripts/mttr-build-ci.sh
@@ -4,16 +4,25 @@ set -ex
 
 . ~/.profile
 
+git submodule update --recursive
+
 mkdir -p "$HOME/deploy_files"
 
 python Tools/autotest/param_metadata/param_parse.py --vehicle ArduCopter
 mv apm.pdef.xml "$HOME/deploy_files"
 
-pushd ArduCopter
-make -j8 px4-v2-mttr-m2
-mv ArduCopter-v2.px4 "$HOME/deploy_files"
-mv ../modules/PX4Firmware/Build/px4fmu-v2_APM.build/firmware.elf "$HOME/deploy_files/fmu.elf"
-mv ../modules/PX4Firmware/Build/px4io-v2_default.build/firmware.elf "$HOME/deploy_files/io.elf"
-make -j8 sitl
-mv ArduCopter.elf "$HOME/deploy_files/sitl.elf"
-popd
+"./modules/waf/waf-light" distclean
+"./modules/waf/waf-light" configure --board px4-v2
+"./modules/waf/waf-light" -j8 --targets bin/arducopter-quad
+mv build/px4-v2/bin/arducopter-quad "$HOME/deploy_files/fmu.elf"
+mv build/px4-v2/bin/arducopter-quad.px4 "$HOME/deploy_files/arducopter-quad.px4"
+
+"./modules/waf/waf-light" distclean
+"./modules/waf/waf-light" configure --board sitl
+"./modules/waf/waf-light" -j8 --targets bin/arducopter-quad
+mv build/sitl/bin/arducopter-quad "$HOME/deploy_files/sitl.elf"
+
+"./modules/waf/waf-light" distclean
+CXX=arm-linux-gnueabihf-g++  CC=arm-linux-gnueabihf-gcc "./modules/waf/waf-light" configure --board sitl
+"./modules/waf/waf-light" -j8 --targets bin/arducopter-quad
+mv build/sitl/bin/arducopter-quad "$HOME/deploy_files/sitl-armhf.elf"
