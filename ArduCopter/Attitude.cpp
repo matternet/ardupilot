3d468e20ef66549ea28de527949c2484716f333a
Jonathan Challinger <mr.challinger@gmail.com>
2016-08-01 16:20:32 -0700

    Copter: disable surface tracking

--- ArduCopter/Attitude.cpp
+++ ArduCopter/Attitude.cpp
@@ -162,7 +162,7 @@ float Copter::get_non_takeoff_throttle()
 //      returns climb rate (in cm/s) which should be passed to the position controller
 float Copter::get_surface_tracking_climb_rate(int16_t target_rate, float current_alt_target, float dt)
 {
-#if RANGEFINDER_ENABLED == ENABLED
+#if 0
     if (!copter.rangefinder_alt_ok()) {
         // if rangefinder is not ok, do not use surface tracking
         return target_rate;
