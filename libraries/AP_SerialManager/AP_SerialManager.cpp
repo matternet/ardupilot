c5a2f196e9025cfccaae92574178d3655b68324d
Jonathan Challinger <mr.challinger@gmail.com>
2016-11-11 11:17:26 -0800

    AP_SerialManager: add Matternet_FTS protocol

--- libraries/AP_SerialManager/AP_SerialManager.cpp
+++ libraries/AP_SerialManager/AP_SerialManager.cpp
@@ -270,6 +270,12 @@ void AP_SerialManager::init()
                     state[i].baud = 115200;
                     state[i].uart->begin(map_baudrate(state[i].baud), 30, 30);
                     state[i].uart->set_flow_control(AP_HAL::UARTDriver::FLOW_CONTROL_DISABLE);
+
+                case SerialProtocol_Matternet_FTS:
+                    state[i].baud = AP_SERIALMANAGER_MATTERNET_FTS_BAUD / 1000;   // update baud param in case user looks at it
+                    state[i].uart->begin(map_baudrate(state[i].baud),
+                                         AP_SERIALMANAGER_MATTERNET_FTS_BUFSIZE_RX,
+                                         AP_SERIALMANAGER_MATTERNET_FTS_BUFSIZE_TX);
                     break;
             }
         }
