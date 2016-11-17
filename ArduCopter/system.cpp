fe0069286da884d965d057b00235f47db0c823df
Jonathan Challinger <mr.challinger@gmail.com>
2016-11-17 15:12:08 -0800

    Copter: call parachute.init

--- ArduCopter/system.cpp
+++ ArduCopter/system.cpp
@@ -232,6 +232,10 @@ void Copter::init_ardupilot()
     camera_mount.init(&DataFlash, serial_manager);
 #endif
 
+#if PARACHUTE == ENABLED
+    parachute.init(serial_manager);
+#endif
+
 #if PRECISION_LANDING == ENABLED
     // initialise precision landing
     init_precland();
