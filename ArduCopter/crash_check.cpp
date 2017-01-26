07cee39bc7e1a13aff6876020bed09bab6c4893b
Jonathan Challinger <mr.challinger@gmail.com>
2017-01-25 18:15:50 -0800

    Copter: parachute deploy WIP

--- ArduCopter/crash_check.cpp
+++ ArduCopter/crash_check.cpp
@@ -54,81 +54,80 @@ void Copter::crash_check()
 #if PARACHUTE == ENABLED
 
 // Code to detect a crash main ArduCopter code
-#define PARACHUTE_CHECK_TRIGGER_SEC         1       // 1 second of loss of control triggers the parachute
-#define PARACHUTE_CHECK_ANGLE_DEVIATION_CD  3000    // 30 degrees off from target indicates a loss of control
+#define PARACHUTE_ANGLE_ERROR_EXCESSIVE_LIMIT_DEG  30.0f
+#define PARACHUTE_ANGLE_ERROR_EXCESSIVE_TIMEOUT_SEC 0.5f
+#define PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_LIMIT_MPS 3.0f
+#define PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_TIMEOUT_SEC 0.5f
 
 // parachute_check - disarms motors and triggers the parachute if serious loss of control has been detected
 // vehicle is considered to have a "serious loss of control" by the vehicle being more than 30 degrees off from the target roll and pitch angles continuously for 1 second
 // called at MAIN_LOOP_RATE
+
 void Copter::parachute_check()
 {
-    static uint16_t control_loss_count;	// number of iterations we have been out of control
-    static int32_t baro_alt_start;
-
-    // exit immediately if parachute is not enabled
+    // Return immediately if parachute is not enabled
     if (!parachute.enabled()) {
+        parachute_check_state.angle_error_excessive = false;
         return;
     }
 
-    // call update to give parachute a chance to move servo or relay back to off position
+    // Update the parachute library
     parachute.update();
 
-    // return immediately if motors are not armed or pilot's throttle is above zero
-    if (!motors->armed()) {
-        control_loss_count = 0;
+    // Return immediately if we are disarmed or landed, or the current mode allows angles beyond the tilt limit
+    if (!motors->armed() || ap.land_complete) {
+        parachute_check_state.angle_error_excessive = false;
         return;
     }
 
-    // return immediately if we are not in an angle stabilize flight mode or we are flipping
-    if (control_mode == ACRO || control_mode == FLIP) {
-        control_loss_count = 0;
-        return;
-    }
-
-    // ensure we are flying
-    if (ap.land_complete) {
-        control_loss_count = 0;
-        return;
-    }
-
-    // ensure the first control_loss event is from above the min altitude
-    if (control_loss_count == 0 && parachute.alt_min() != 0 && (current_loc.alt < (int32_t)parachute.alt_min() * 100)) {
-        return;
-    }
-
-    // check for angle error over 30 degrees
+    // Retrieve useful values
     const float angle_error = attitude_control->get_att_error_angle_deg();
-    if (angle_error <= CRASH_CHECK_ANGLE_DEVIATION_DEG) {
-        if (control_loss_count > 0) {
-            control_loss_count--;
+    const float tilt_angle = acosf(ahrs.get_rotation_body_to_ned().c.z);
+    const float tilt_angle_limit = attitude_control->get_tilt_limit_rad() + radians(PARACHUTE_ANGLE_ERROR_EXCESSIVE_LIMIT_DEG);
+    const float vel_z = -inertial_nav.get_velocity_z()*0.01f; // Convert from NEU to NED
+    const float vel_z_error = -pos_control->get_vel_error_z()*0.01f; // Convert from NEU to NED
+
+    // Check if the angle error is excessive, start the timer when it becomes excessive
+    bool new_angle_error_excessive = angle_error > PARACHUTE_ANGLE_ERROR_EXCESSIVE_LIMIT_DEG;
+    if (new_angle_error_excessive && !parachute_check_state.angle_error_excessive) {
+        parachute_check_state.angle_error_excessive_begin_ms = millis();
+    }
+    parachute_check_state.angle_error_excessive = new_angle_error_excessive;
+
+    // Timeout occurs if angle error is excessive and PARACHUTE_ANGLE_ERROR_EXCESSIVE_TIMEOUT_SEC has elapsed
+    bool angle_error_excessive_timeout = parachute_check_state.angle_error_excessive && (millis()-parachute_check_state.angle_error_excessive_begin_ms)*1e-3f > PARACHUTE_ANGLE_ERROR_EXCESSIVE_TIMEOUT_SEC;
+
+    // Check if the tilt angle is excessive
+    bool tilt_angle_excessive = parachute_check_state.angle_error_excessive && tilt_angle > tilt_angle_limit;
+
+    // Check if the there is excessive vertical velocity error, start the timer when it becomes excessive
+    bool new_vel_z_error_excessive = pos_control->is_active_z() && fabsf(vel_z) > PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_LIMIT_MPS && fabsf(vel_z_error) > PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_LIMIT_MPS;
+    if (new_vel_z_error_excessive && !parachute_check_state.vel_z_error_excessive) {
+        parachute_check_state.vel_z_error_excessive_begin_ms = millis();
+    }
+    parachute_check_state.vel_z_error_excessive = new_vel_z_error_excessive;
+
+    // Timeout occurs if vertical velocity error is excessive and PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_TIMEOUT_SEC has elapsed
+    bool vel_z_error_excessive_timeout = parachute_check_state.vel_z_error_excessive && (millis()-parachute_check_state.vel_z_error_excessive_begin_ms)*1e-3f > PARACHUTE_VERT_VEL_ERROR_EXCESSIVE_TIMEOUT_SEC;
+
+    // Special case: in stabilize, throttle cut completely, falling
+    bool stabilize_throttle_cut = control_mode == STABILIZE && ap.throttle_zero && vel_z > 5.0f;
+
+    if (angle_error_excessive_timeout || tilt_angle_excessive || vel_z_error_excessive_timeout || stabilize_throttle_cut) {
+        uint8_t errcode = ERROR_CODE_UNHEALTHY;
+        if (angle_error_excessive_timeout) {
+            errcode = ERROR_CODE_PARACHUTE_REASON_ANGLE_ERROR_EXCESSIVE_TIMEOUT;
+        } else if (tilt_angle_excessive) {
+            errcode = ERROR_CODE_PARACHUTE_REASON_TILT_ANGLE_EXCESSIVE;
+        } else if (vel_z_error_excessive_timeout) {
+            errcode = ERROR_CODE_PARACHUTE_REASON_VEL_Z_ERROR_EXCESSIVE_TIMEOUT;
+        } else if (stabilize_throttle_cut) {
+            errcode = ERROR_CODE_PARACHUTE_REASON_STABILIZE_THROTTLE_CUT;
         }
-        return;
-    }
 
-    // increment counter
-    if (control_loss_count < (PARACHUTE_CHECK_TRIGGER_SEC*scheduler.get_loop_rate_hz())) {
-        control_loss_count++;
-    }
-
-    // record baro alt if we have just started losing control
-    if (control_loss_count == 1) {
-        baro_alt_start = baro_alt;
-
-    // exit if baro altitude change indicates we are not falling
-    } else if (baro_alt >= baro_alt_start) {
-        control_loss_count = 0;
-        return;
-
-    // To-Do: add check that the vehicle is actually falling
-
-    // check if loss of control for at least 1 second
-    } else if (control_loss_count >= (PARACHUTE_CHECK_TRIGGER_SEC*scheduler.get_loop_rate_hz())) {
-        // reset control loss counter
-        control_loss_count = 0;
-        // log an error in the dataflash
-        Log_Write_Error(ERROR_SUBSYSTEM_CRASH_CHECK, ERROR_CODE_CRASH_CHECK_LOSS_OF_CONTROL);
-        // release parachute
+        Log_Write_Error(ERROR_SUBSYSTEM_PARACHUTE, errcode);
         parachute_release();
+        return;
     }
 }
 
