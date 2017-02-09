17d0b58740780eb38307ae467e86f85d383f9e5a
Jonathan Challinger <mr.challinger@gmail.com>
2017-02-09 12:07:25 -0800

    Copter: add parachute deployment option to EKF failsafe

--- ArduCopter/ekf_check.cpp
+++ ArduCopter/ekf_check.cpp
@@ -164,6 +164,12 @@ void Copter::failsafe_ekf_event()
                 set_mode_land_with_pause(ModeReason::EKF_FAILSAFE);
             }
             break;
+
+        case FS_EKF_ACTION_PARACHUTE:
+            Log_Write_Error(ERROR_SUBSYSTEM_PARACHUTE, ERROR_CODE_PARACHUTE_REASON_EKF_FAILSAFE);
+            parachute_release();
+            break;
+
         case FS_EKF_ACTION_LAND:
         case FS_EKF_ACTION_LAND_EVEN_STABILIZE:
         default:
