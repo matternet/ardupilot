ae27d693b981f5465334930f93e654ca6b8d3b19
Jonathan Challinger <mr.challinger@gmail.com>
2017-04-14 14:30:37 -0700

    AP_Motors: add frame type MATTERNET_M2

--- libraries/AP_Motors/AP_Motors_Class.h
+++ libraries/AP_Motors/AP_Motors_Class.h
@@ -59,6 +59,7 @@ public:
         MOTOR_FRAME_TYPE_DJI_X = 13, // X frame, DJI ordering
         MOTOR_FRAME_TYPE_CW_X = 14, // X frame, clockwise ordering
         MOTOR_FRAME_TYPE_I = 15, // (sideways H) octo only
+        MOTOR_FRAME_TYPE_MATTERNET_M2 = 100,
     };
 
     // Constructor
