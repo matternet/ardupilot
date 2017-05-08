aff1231ad0109077f048049ed1dfaa95b1f6e16d
Jonathan Challinger <mr.challinger@gmail.com>
2017-05-08 14:35:50 -0700

    AC_WPNav: modify leash length computation

--- libraries/AC_AttitudeControl/AC_PosControl.h
+++ libraries/AC_AttitudeControl/AC_PosControl.h
@@ -297,6 +297,9 @@ public:
     // time_since_last_xy_update - returns time in seconds since the horizontal position controller was last run
     float time_since_last_xy_update() const;
 
+    /// calc_leash_length - calculates the horizontal leash length given a maximum speed, acceleration and position kP gain
+    float calc_leash_length(float speed_cms, float accel_cms, float kP) const;
+
     static const struct AP_Param::GroupInfo var_info[];
 
 protected:
@@ -363,9 +366,6 @@ protected:
     /// accel_to_lean_angles - horizontal desired acceleration to lean angles
     ///    converts desired accelerations provided in lat/lon frame to roll/pitch angles
     void accel_to_lean_angles(float dt_xy, float ekfNavVelGainScaler, bool use_althold_lean_angle);
-
-    /// calc_leash_length - calculates the horizontal leash length given a maximum speed, acceleration and position kP gain
-    float calc_leash_length(float speed_cms, float accel_cms, float kP) const;
 
     /// initialise and check for ekf position resets
     void init_ekf_xy_reset();
