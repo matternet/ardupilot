3e706a0c88109d303415257ba3c02c73158d74c9
Jonathan Challinger <mr.challinger@gmail.com>
2017-06-27 16:54:04 -0700

    Copter: add pre-arm check for rangefinders that are configured but not functional

--- libraries/AP_RangeFinder/RangeFinder.h
+++ libraries/AP_RangeFinder/RangeFinder.h
@@ -147,10 +147,6 @@ public:
     MAV_DISTANCE_SENSOR get_mav_distance_sensor_type_orient(enum Rotation orientation) const;
     RangeFinder_Status status_orient(enum Rotation orientation) const;
 
-    bool configured(uint8_t instance) const {
-        return get_backend(instance) != nullptr;
-    }
-
     bool has_data_orient(enum Rotation orientation) const;
     uint8_t range_valid_count_orient(enum Rotation orientation) const;
     const Vector3f &get_pos_offset_orient(enum Rotation orientation) const;
