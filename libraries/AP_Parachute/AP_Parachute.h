a99d549d83e91af6e25b25ecacd255a4182690cf
Jonathan Challinger <mr.challinger@gmail.com>
2017-07-07 13:56:13 -0700

    AP_Parachute: add get_mttr_prearm_pass

--- libraries/AP_Parachute/AP_Parachute.h
+++ libraries/AP_Parachute/AP_Parachute.h
@@ -71,6 +71,7 @@ public:
     int16_t alt_min() const { return _alt_min; }
 
     const char* mttr_get_fts_version() { return _mttr_fts_version; }
+    bool get_mttr_prearm_pass() { return _mttr_prearm_pass; }
 
     static const struct AP_Param::GroupInfo        var_info[];
 
@@ -92,6 +93,8 @@ private:
 
     // Matternet FTS
     uint32_t _mttr_last_loop_ms;
+    uint32_t _mttr_last_status_recv_ms;
+    bool _mttr_prearm_pass;
     AP_HAL::UARTDriver *_mttr_uart = nullptr;
     char _mttr_fts_version[16];
     void send_debug_message(uint32_t tnow_ms, uint8_t ind, float value);
