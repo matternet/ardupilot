1398b7573c50f15fe9535ad2082ff71cc59fef8b
MitchKoch <mitch@matternet.us>
2017-07-07 18:09:53 -0700

    AP_RangeFinder: Update LightWareI2C rangefinder to autodetect and support the FX20/LW20 with ASCII init. - Add fx20 identification to AP_RangeFinder_LightWareI2C::init function. - Refactor AP_RangeFinder_LightWareI2C::get_reading() to AP_RangeFinder_LightWareI2C::legacy_get_reading. - Add AP_RangeFinder_LightWareI2C::fx20_get_reading

--- libraries/AP_RangeFinder/AP_RangeFinder_LightWareI2C.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_LightWareI2C.h
@@ -25,10 +25,20 @@ private:
     // constructor
     AP_RangeFinder_LightWareI2C(RangeFinder::RangeFinder_State &_state, AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev);
 
+    bool write_bytes(uint8_t *write_buf_u8, uint32_t len_u8);
+    bool fx20_disable_address_tagging();
+    bool fx20_product_name_check();
+    bool fx20_send_and_expect(const char* send, const char* expected_reply);
+    bool fx20_set_lost_signal_confirmations();
+    bool fx20_wait_on_reply(uint8_t *rx_two_bytes);
     bool init();
-    void timer();
+    bool legacy_init();
+    bool fx20_init();
+    void legacy_timer();
+    void fx20_timer();
 
     // get a reading
-    bool get_reading(uint16_t &reading_cm);
+    bool legacy_get_reading(uint16_t &reading_cm);
+    bool fx20_get_reading(uint16_t &reading_cm);
     AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
 };
