e42f57c17ac7aa0e96b46f08aa373fc494f9e18f
Jonathan Challinger <mr.challinger@gmail.com>
2017-07-17 12:01:04 -0700

    AP_Parachute: add fuse fault to Matternet FTS prearm

--- libraries/AP_Parachute/AP_Parachute.h
+++ libraries/AP_Parachute/AP_Parachute.h
@@ -34,7 +34,8 @@ public:
         , _release_initiated(false)
         , _release_in_progress(false)
         , _released(false)
-        , _mttr_prearm_pass(false)
+        , _mttr_status_pass(false)
+        , _mttr_fuse_pass(true)
     {
         // setup parameter defaults
         AP_Param::setup_object_defaults(this, var_info);
@@ -76,7 +77,7 @@ public:
     int16_t alt_min() const { return _alt_min; }
 
     const char* mttr_get_fts_version() { return _mttr_fts_version; }
-    bool get_mttr_prearm_pass() { return _mttr_prearm_pass; }
+    bool get_mttr_prearm_pass() { return _mttr_status_pass && _mttr_fuse_pass; }
 
     static const struct AP_Param::GroupInfo        var_info[];
 
@@ -99,7 +100,8 @@ private:
     // Matternet FTS
     uint32_t _mttr_last_loop_ms;
     uint32_t _mttr_last_status_recv_ms;
-    bool _mttr_prearm_pass;
+    bool _mttr_status_pass;
+    bool _mttr_fuse_pass;
     AP_HAL::UARTDriver *_mttr_uart = nullptr;
     char _mttr_fts_version[16];
     void send_debug_message(uint32_t tnow_ms, uint8_t ind, float value);
