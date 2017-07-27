2b73ea57b86c73635f06d97eb2024fb3936345e7
Jonathan Challinger <mr.challinger@gmail.com>
2017-07-27 10:49:47 -0700

    AC_PrecLand: add acceleration bias learning

--- libraries/AC_PrecLand/PosVelEKF.h
+++ libraries/AC_PrecLand/PosVelEKF.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 class PosVelEKF {
 public:
     void init(float pos, float posVar, float vel, float velVar);
@@ -7,12 +9,16 @@ public:
     void fusePos(float pos, float posVar);
     void fuseVel(float vel, float velVar);
 
-    float getPos() const { return _state[0]; }
-    float getVel() const { return _state[1]; }
+    float getPos() const { return _state[_state_idx].x[0]; }
+    float getVel() const { return _state[_state_idx].x[1]; }
+    float getABias() const { return _state[_state_idx].x[2]; }
 
     float getPosNIS(float pos, float posVar);
 
 private:
-    float _state[2];
-    float _cov[3];
+    struct {
+        float x[3];
+        float P[6];
+    } _state[2];
+    uint8_t _state_idx;
 };
