6d135259dca74229ef7bd31d67fcd9b68f131db6
Jonathan Challinger <mr.challinger@gmail.com>
2018-07-12 08:13:22 -0700

    build: travis builds fmuv3

--- Tools/scripts/mttr-build-ci.sh
+++ Tools/scripts/mttr-build-ci.sh
@@ -9,17 +9,14 @@ mkdir -p "$HOME/deploy_files"
 python Tools/autotest/param_metadata/param_parse.py --vehicle ArduCopter
 mv apm.pdef.xml "$HOME/deploy_files"
 
-# export PX4_PX4IO_NAME="px4io-v2"
-
 unset CXX CC
 
 ./waf distclean
-./waf configure --board=px4-v3
+./waf configure --board=fmuv3
 ./waf build --targets bin/arducopter -j8
 ./waf configure --board=sitl
 ./waf build --targets bin/arducopter -j8
 
-mv build/px4-v3/bin/arducopter.px4 "$HOME/deploy_files"
-mv build/px4-v3/bin/arducopter "$HOME/deploy_files/fmu.elf"
-mv build/px4-v3/px4-extra-files/px4io "$HOME/deploy_files/io.elf"
+mv build/fmuv3/bin/arducopter.apj "$HOME/deploy_files"
+mv build/fmuv3/bin/arducopter "$HOME/deploy_files/fmu.elf"
 mv build/sitl/bin/arducopter "$HOME/deploy_files/sitl.elf"
