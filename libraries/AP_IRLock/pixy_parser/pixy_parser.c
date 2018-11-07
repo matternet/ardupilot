9f1cced2842c07803921345c956f6590da99fb59
Akshay Bajaj <akshay@matternet.us>
2018-11-07 14:34:20 -0800

    Tested more edge cases and added a feature which tell how many bytes are needed to complete a block/SOF

--- libraries/AP_IRLock/pixy_parser/pixy_parser.c
+++ libraries/AP_IRLock/pixy_parser/pixy_parser.c
@@ -1,3 +1,5 @@
+    ///---------------------- Check all the " . " that were replaced with  " -> " ---------------------------------------///
+
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
@@ -5,7 +7,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define PIXY_BUF_SIZE 32
+#define PIXY_BUF_SIZE 17
 
 enum message_validity_t {
     MESSAGE_EMPTY,
@@ -17,10 +19,9 @@ enum message_validity_t {
 
 static uint8_t pixy_buf[PIXY_BUF_SIZE];
 static size_t pixy_len;
-static size_t sof_one_byte_index;
-static size_t block_one_byte_index;
 static uint8_t blob_buffer_write_idx;
-
+static size_t bytes_to_sof;
+static size_t bytes_to_block;
 
 typedef struct {
     uint16_t center_x;
@@ -36,30 +37,53 @@ static struct blob_buffer {    //Frame (full of blobs)
 } blob_buffer[2];
 
 
+void empty_pixyBuf() {
+    for (size_t i=0; i<pixy_len; i++) {
+        pixy_buf[i] = 0;    
+    }
+    pixy_len = 0;
+}
+
+
+void print_buffer() {
+    struct blob_buffer& writebuf = blob_buffer[blob_buffer_write_idx];
+    printf("Buffer:");  
+//    printf("[%u, %u, %u, %u], ", (unsigned)writebuf.blobs[writebuf.count].center_x, (unsigned)writebuf.blobs[writebuf.count].center_y, (unsigned)writebuf.blobs[writebuf.count].width, (unsigned)writebuf.blobs[writebuf.count].height);
+    printf("[%u, %u, %u, %u], ", (unsigned)writebuf.blobs[1].center_x, (unsigned)writebuf.blobs[1].center_y, (unsigned)writebuf.blobs[1].width, (unsigned)writebuf.blobs[1].height);
+    printf("   -   Count: %u\n", (unsigned)writebuf.count);
+}
+
 
 // - Write Buffer
 bool write_buffer(const pixy_blob& blob1) {
+    printf("Writing Main Buffer: Writing to %u\n", blob_buffer_write_idx);
 //    struct blob_buffer* writebuf = &blob_buffer[blob_buffer_write_idx];
     struct blob_buffer& writebuf = blob_buffer[blob_buffer_write_idx];
     if (writebuf.count >= 10) {
         return false;
     }
     writebuf.blobs[writebuf.count] = blob1;
+    printf("SEE THIS----   [%u, %u, %u, %u], ", (unsigned)blob1.center_x, (unsigned)blob1.center_y, (unsigned)blob1.width, (unsigned)blob1.height);
     writebuf.count++;
+    print_buffer();
     return true;
 }
 
 // Blob buffer indexing swap
 void swap_buffer() {
     blob_buffer_write_idx = (blob_buffer_write_idx+1)%2;
+    printf("Swapping Main Buffer: Swapped to %u\n", blob_buffer_write_idx);
 //    struct blob_buffer* writebuf = &blob_buffer[blob_buffer_write_idx];
     struct blob_buffer& writebuf = blob_buffer[blob_buffer_write_idx];
     writebuf.count = 0;        //This is probably wront. But helps me get rid of an error!!!-----------------------------------------------------------
+    print_buffer();
 }
+
 
 // - read blob i:
 
 const pixy_blob* read_buffer(size_t i) {
+    printf("Reading Main Buffer: Reading from %u\n", (blob_buffer_write_idx+1)%2);
 //    struct blob_buffer* readbuf = &blob_buffer[(blob_buffer_write_idx+1)%2];
     struct blob_buffer& readbuf = blob_buffer[(blob_buffer_write_idx+1)%2];
     if (i >= readbuf.count) {
@@ -69,7 +93,7 @@ const pixy_blob* read_buffer(size_t i) {
 }
 
 static enum message_validity_t check_pixy_message(size_t pixy_len) {
-    printf("INSIDE CHECK_PIXY_MSG:%u\n", (unsigned)pixy_len);
+//    printf("INSIDE CHECK_PIXY_MSG:%u\n", (unsigned)pixy_len);
 
     if (pixy_len == 0) {
         return MESSAGE_EMPTY;
@@ -81,6 +105,24 @@ static enum message_validity_t check_pixy_message(size_t pixy_len) {
     if (pixy_len >= 2 && pixy_buf[1] != 0xAA) {   //checking if 2nd byte of the message is 0xAA   (Combined it verifies if the message is 0x55AA)
         printf("Second block not 0xAA .. Message Invalid!\n");
         return MESSAGE_INVALID;
+    }
+    if (pixy_buf[2] == 0x55 && pixy_buf[3] == 0xAA) {
+        bytes_to_sof = 16 - pixy_len;
+        if (bytes_to_sof == 0) {
+            printf("SOF COMPLETE!\n");    
+        } 
+        else {
+            printf("Need %u more bytes to complete a SOF\n", (unsigned)bytes_to_sof);
+        }
+    }
+    if (pixy_buf[2] != 0x55 && pixy_buf[3] != 0xAA) {
+        bytes_to_block = 14 - pixy_len;
+        if (bytes_to_block == 0) {
+            printf("BLOCK COMPLETE!\n");    
+        } 
+        else {
+            printf("Need %u more bytes to complete a Block\n", (unsigned)bytes_to_sof);
+        }
     }
     if (pixy_len < 14) {     // Looks for at least 14 bytes i.e. "sync+blob_data" = 2+12 bytes)  
         printf("Message Length less than 14.. Message Incomplete!\n");
@@ -134,23 +176,27 @@ static enum message_validity_t check_pixy_message(size_t pixy_len) {
 }
 
 void recv_byte_pixy(uint8_t byte) {
-    printf("INSIDE recv_byte_pixy:-\n");
+//    printf("INSIDE recv_byte_pixy:-\n");
 
 // Read 2 bytes
-    pixy_blob blob1;    
+    pixy_blob blob1;    //%%%%%%%%%%%%%%%%%%%%$$$$$$$$$$$$$$$$$$$$$$$$-------------- should I put struct as prefix or not?-------------------------%%%%%%%%%$$$$$$$$$
+
     struct blob_buffer& writebuf = blob_buffer[blob_buffer_write_idx];
 
-    printf("The byte inputted is: %u\n", byte);
+    printf("The byte inputted is: %u and pixylen is %u\n", byte, (unsigned)pixy_len);
     pixy_buf[pixy_len++] = byte;        // Append byte to buffer
 
-    enum message_validity_t validity = check_pixy_message(pixy_len);   
-    printf("Validity: %d (0:empty 1:invalid 2:incomplete 3:valid_SOF 4:valid_block)\n", validity);
+    enum message_validity_t validity = check_pixy_message(pixy_len);    // Call parser 
+    printf("Validity Check Call: %d (0:empty 1:invalid 2:incomplete 3:valid_SOF 4:valid_block)\n", validity);
     printf("Pixy Len: %u\n", (unsigned)pixy_len);
-    printf("Pixy_buf: %d\n", pixy_buf);
+    printf("Pixy_buf: ");   
+    for (size_t i=0; i<17; i++) {
+        printf("%u, ", pixy_buf[i]);
+    }   
+    printf("\n");
 
     if (validity == MESSAGE_VALID_SOF) {
 //        pixy_frame_received(pixy_len, pixy_buf);
-        pixy_len = 0;
         
         if (!(writebuf.count >= 10)) {
             if (writebuf.count != 0) {
@@ -162,24 +208,41 @@ void recv_byte_pixy(uint8_t byte) {
                 blob1.height = pixy_buf[14] | (uint16_t)pixy_buf[15]<<8;
 
                 writebuf.count = 0;     //Turn the count to 0
+                printf("@@@SEE THIS----   [%u, %u, %u, %u], ", (unsigned)blob1.center_x, (unsigned)blob1.center_y, (unsigned)blob1.width, (unsigned)blob1.height);
 
                 write_buffer(blob1);  //Writing inside the buffer(the count increment happens inside the write_buffer())
+                empty_pixyBuf();    //Emptying The pixy_buf once read the SOF into 'writebuf'
+
             }
             else {
                 write_buffer(blob1);    //Writing inside the buffer(the count increment happens inside the write_buffer())
+                empty_pixyBuf();    //Emptying The pixy_buf once read the SOF into 'writebuf'
+
+                printf("\n--------------------------AFTER EMPTY: ");
+                for (int i=0; i<pixy_len; i++) {
+                    printf("%u, ", (unsigned)pixy_buf[i]);
+                }
             }
         }
     }
 
-    if (validity == MESSAGE_VALID_BLOCK) {    
+    if (validity == MESSAGE_VALID_BLOCK) {     ///-----------------------------how can I store the block inside a frame?-----------------
         if (!(writebuf.count >= 10)) {
             blob1.center_x = pixy_buf[6] | (uint16_t)pixy_buf[7]<<8; //Extract blob information
             blob1.center_x = pixy_buf[8] | (uint16_t)pixy_buf[9]<<8; 
             blob1.width = pixy_buf[10] | (uint16_t)pixy_buf[11]<<8;
             blob1.height = pixy_buf[12] | (uint16_t)pixy_buf[13]<<8;
+            printf("@@@SEE THIS----   [%u, %u, %u, %u], ", (unsigned)blob1.center_x, (unsigned)blob1.center_y, (unsigned)blob1.width, (unsigned)blob1.height);
 
             write_buffer(blob1);  //Writing inside the buffer
-        } 
+            empty_pixyBuf();    //Emptying The pixy_buf once read the SOF into 'writebuf'
+
+            printf("\n------------------------------AFTER EMPTY: ");
+            for (int i=0; i<pixy_len; i++) {
+                printf("%u, ", (unsigned)pixy_buf[i]);
+            }
+
+        }
     }
 
     if (validity == MESSAGE_INVALID) {  // If message invalid, wait and read 2 bytes
@@ -194,11 +257,18 @@ void recv_byte_pixy(uint8_t byte) {
 }
 
 int main() {
-    uint8_t input_bytes[] = {0x55, 0xAA, 0x55, 0xAA, 0x0F, 0x01, 0x01, 0x00, 0x68, 0x00, 0x52, 0x00, 0x24, 0x00, 0x30, 0x00, 0x55, 0xAA, 0x90, 0x01, 0x01, 0x00, 0xE5, 0x00, 0x56, 0x00, 0x25, 0x00, 0x2F, 0x00, 0x55, 0xAA, 0x55, 0xAA, 0x17, 0x01, 0x01, 0x00, 0x69, 0x00, 0x56, 0x00, 0x27, 0x00, 0x30, 0x00, 0x55, 0xAA, 0x17, 0x01, 0xF1, 0xE0, 0x09, 0x10, 0x46, 0x00, 0x27, 0xE0, 0x00, 0xF0};  //array of 15 bytes of SOF 
+    uint8_t input_bytes[] = {0x55, 0xAA, 0x55, 0xAA, 0x0F, 0x01, 0x01, 0x00, 0x68, 0x00, 0x52, 0x00, 0x24, 0x00, 0x30, 0x00, 0x55, 0xAA, 0x90, 0x01, 0x01, 0x00, 0xE5, 0x00, 0x56, 0x00, 0x25, 0x00, 0x2F, 0x00, 0x55, 0xAA, 0x55, 0xAA, 0x17, 0x01, 0x01, 0x00, 0x69, 0x00, 0x56, 0x00, 0x27, 0x00, 0x30, 0x00, 0x55, 0xAA, 0x17, 0x01, 0xF1, 0xE0, 0x09, 0x10, 0x46, 0x00, 0x27, 0xE0, 0x00, 0xF0, 0x55, 0xAA, 0x90, 0x01, 0x01, 0x00, 0xE5, 0x00, 0x56, 0x00, 0x25, 0x00, 0x2F, 0x00, 0x55, 0xAA, 0x90, 0x01, 0x01, 0x00, 0xE5, 0x00, 0x56, 0x00, 0x25, 0x00, 0x2F, 0x00, 0x55, 0xAA, 0x00, 0x01, 0xF1, 0x0A, 0xE0, 0x10, 0x55, 0x00, 0x15, 0x11, 0x2F, 0x00, 0x55, 0xAA, 0x55, 0xAA, 0x0F, 0x01, 0x01, 0x00, 0x68, 0x00, 0x52, 0x00, 0x24, 0x00, 0x30, 0x00};  //array of 15 bytes of SOF 
+//    uint8_t input_bytes[] = {85,  170,  85,   170,  15,   1,    1,    0,    104,   0,    82,   0,   36,    0,   48,   0,    85,  170,  144,   1,     1,    0,   229,   0,   86,    0,   37,   0,    47,    0,   85,   170,   85,  170,  23,    1,    1,   0,    105,  0,     86,   0,    39,   0,   48,    0,   85,   170,  23,   1,   241,  224,   9,    16,   70,    0,   39,   224,  0,   240,  124   };  //array of 15 bytes of SOF 
+
+    for (size_t i=0; i<=sizeof(input_bytes); i++) {
+        printf("%u, ", (unsigned)input_bytes[i]);
+    }
+
     uint8_t input_bytes2[] = {};
     for (size_t i=0; i<=sizeof(input_bytes); i++) {
-        printf("Calling recv_byte_pixy : %d       :  %u\n", i, (unsigned)input_bytes[i]);
+//        printf("Calling recv_byte_pixy : %d       :  %u\n", i, (unsigned)input_bytes[i]);
+        printf("\nNEW BYTE IN:\n");
         recv_byte_pixy(input_bytes[i]);
     }
     return 0;
-}   
+}
