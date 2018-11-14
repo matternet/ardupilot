8dba09c4656349db660f44dc1276299763b36298
Jonathan Challinger <mr.challinger@gmail.com>
2018-11-14 09:08:01 -0800

    Copter: run compass_checks in arming checks

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -574,6 +574,10 @@ bool AP_Arming_Copter::arm_checks(bool display_failure, bool arming_from_gcs)
         }
         return false;
     }
+    
+    if (!compass_checks(display_failure)) {
+        return false;
+    }
 
     if ((checks_to_perform == ARMING_CHECK_ALL) || (checks_to_perform & ARMING_CHECK_COMPASS)) {
         // check compass offsets have been set.  AP_Arming only checks
