dbbed62d8ad7deae4c867b9e2de3d1a04d75e220
Akshay Bajaj <akshay@matternet.us>
2018-11-26 14:11:28 -0800

    Refactored the code to OOP, still needs debugging

--- libraries/AP_IRLock/pixy_parser/pixy_parser.h
+++ libraries/AP_IRLock/pixy_parser/pixy_parser.h
@@ -4,21 +4,31 @@
  */
 
 #pragma once
+
 #include <ctype.h>
-#include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
-#define PIXY_BUF_SIZE 17
-    
-
+/*
+    pixy_buf_size = 17;
+    pixy_buf[17]= {};
+    pixy_len = 0;
+    blob_buffer_write_idx = 0;
+    bytes_to_sof = 0;
+    bytes_to_block = 0;
+}
+*/
 class pixy_parser {
     public:
-        pixy_parser();
+        pixy_parser(size_t, uint8_t[], size_t, uint8_t, size_t, size_t);
         ~pixy_parser();
         void empty_pixyBuf(void);
         void print_buffer(void);
         void swap_buffer(void);
-    
         void recv_byte_pixy(uint8_t byte);
 
     private:
@@ -29,22 +39,30 @@ class pixy_parser {
             uint16_t height;
         } pixy_blob;
 
-        static struct blob_buffer {    //Frame (full of blobs)   
+        struct blob_buffer {    //Frame (full of blobs)
             pixy_blob blobs[10];
             size_t count;
         } blob_buffer[2];
 
-        bool write_buffer(const pixy_blob& blob1);
-    
-        const pixy_blob* read_buffer(size_t i);
 
-        uint8_t pixy_buf[PIXY_BUF_SIZE];
+        enum message_validity_t {
+            MESSAGE_EMPTY,
+            MESSAGE_INVALID,
+            MESSAGE_INCOMPLETE,
+            MESSAGE_VALID_SOF,
+            MESSAGE_VALID_BLOCK
+        };
+
+        bool write_buffer(const pixy_blob& blob1);
+        const pixy_blob* read_buffer(size_t i);
+        enum message_validity_t check_pixy_message(size_t pixy_len);
+
+        size_t pixy_buf_size;
+        uint8_t pixy_buf[17];
         size_t pixy_len;
         uint8_t blob_buffer_write_idx;
         size_t bytes_to_sof;
         size_t bytes_to_block;
-
-
 };
 
 
