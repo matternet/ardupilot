f73a0e93abba7a8cc27ad51fa31bf45c843a9483
Akshay Bajaj <akshay@matternet.us>
2018-11-28 18:24:53 -0800

    Integrated pixy_paser with the firmware, still doubt the integrity of the output vals(x,y,w,h)

--- libraries/AP_IRLock/AP_IRLock_I2C.h
+++ libraries/AP_IRLock/AP_IRLock_I2C.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "IRLock.h"
+#include "pixy_parser.h"
 
 class AP_IRLock_I2C : public IRLock
 {
@@ -15,25 +16,12 @@ public:
     // retrieve latest sensor data - returns true if new data is available
     bool update() override;
 
+    pixy_parser pixyObj;
+
 private:
     AP_HAL::OwnPtr<AP_HAL::Device> dev;
 
-    struct PACKED frame {
-        uint16_t checksum;
-        uint16_t signature;
-        uint16_t pixel_x;
-        uint16_t pixel_y;
-        uint16_t pixel_size_x;
-        uint16_t pixel_size_y;
-    };
-
-    bool timer(void);
-
-    bool sync_frame_start(void);
-    bool sync_frame_once(void);
-    bool read_block(struct frame &irframe);
     void read_frames(void);
-
     void pixel_to_1M_plane(float pix_x, float pix_y, float &ret_x, float &ret_y);
 
     AP_HAL::Semaphore *sem;
