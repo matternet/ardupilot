be9082fee2fb7302ae2fe01e76285ca9756eee1e
Andrew Tridgell <andrew@tridgell.net>
2019-03-08 14:14:47 +1100

    AP_SerialManager: fixed missing break

--- libraries/AP_SerialManager/AP_SerialManager.cpp
+++ libraries/AP_SerialManager/AP_SerialManager.cpp
@@ -270,6 +270,7 @@ void AP_SerialManager::init()
                     state[i].baud = 115200;
                     state[i].uart->begin(map_baudrate(state[i].baud), 30, 30);
                     state[i].uart->set_flow_control(AP_HAL::UARTDriver::FLOW_CONTROL_DISABLE);
+                    break;
 
                 case SerialProtocol_Matternet_FTS:
                     state[i].baud = AP_SERIALMANAGER_MATTERNET_FTS_BAUD / 1000;   // update baud param in case user looks at it
