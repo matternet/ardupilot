e5df1586b986abcfde745030007f4cdc2494bbfd
Andrew Tridgell <andrew@tridgell.net>
2019-05-07 15:08:10 +1000

    AP_Notify: updated buzzer tones to match previous master-0.4
    fixes jira APM-8

--- libraries/AP_Notify/ToneAlarm.cpp
+++ libraries/AP_Notify/ToneAlarm.cpp
@@ -88,7 +88,7 @@ const AP_ToneAlarm::Tone AP_ToneAlarm::_tones[] {
 #define AP_NOTIFY_TONE_QUIET_NOT_READY_OR_NOT_FINISHED 28
     { "MFT200L4<B#4A#6G#6", false },
 #define AP_NOTIFY_TONE_STARTUP 29
-    { "MFT240L8O4aO5dcO4aO5dcO4aO5dcL16dcdcdcdc", false },
+    { "MFMST200L64O4ceceP32ceceP8df#df#P32df#df#P8L16gf#g>c", false },
 };
 
 bool AP_ToneAlarm::init()
@@ -289,6 +289,8 @@ void AP_ToneAlarm::update()
         }
     }
 
+#if 0
+    // disabled for matternet
     // notify the user when pre_arm checks are passing
     if (flags.pre_arm_check != AP_Notify::flags.pre_arm_check) {
         flags.pre_arm_check = AP_Notify::flags.pre_arm_check;
@@ -302,6 +304,7 @@ void AP_ToneAlarm::update()
             }
         }
     }
+#endif
 
     // check if arming status has changed
     if (flags.armed != AP_Notify::flags.armed) {
