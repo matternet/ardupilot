e922ed0b741e10b2c0db8597ab2cda8aeb314a1d
Andrew Tridgell <andrew@tridgell.net>
2019-06-18 20:08:49 +1000

    AP_BattMonitor: added reset_remaining() API
    used to allow battery swap before takeoff

--- libraries/AP_BattMonitor/AP_BattMonitor_Backend.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_Backend.h
@@ -53,6 +53,9 @@ public:
     virtual void handle_bi_msg(float voltage, float current,
             float temperature) {}
 
+    // reset remaining percentage to given value
+    virtual bool reset_remaining(float percentage);
+
 protected:
     AP_BattMonitor                      &_mon;      // reference to front-end
     AP_BattMonitor::BattMonitor_State   &_state;    // reference to this instances state (held in the front-end)
