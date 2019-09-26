dc9c682ec7c11ccab0f4b5986eda2ef14aad40b5
Andrew Tridgell <andrew@tridgell.net>
2019-09-26 10:54:06 +1000

    AP_BattMonitor: added battery monitor backend based on SoC table
    this uses BATT_MONITOR=20 to select a table based battery monitor
    where the table is used when disarmed and the integration of current
    when armed
    
    The table is specific to a particular battery type

---
+++ libraries/AP_BattMonitor/AP_BattMonitor_Analog_Table.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "AP_BattMonitor_Analog.h"
+
+class AP_BattMonitor_Analog_Table : public AP_BattMonitor_Analog
+{
+public:
+    /// Constructor
+    AP_BattMonitor_Analog_Table(AP_BattMonitor &mon, AP_BattMonitor::BattMonitor_State &mon_state, AP_BattMonitor_Params &params) :
+        AP_BattMonitor_Analog(mon, mon_state, params) {}
+
+
+    /// Read the battery voltage and current.  Should be called at 10hz
+    void read() override;
+
+    /// returns true if battery monitor provides current info
+    bool has_current() const override { return true; }
+    
+private:
+    uint32_t last_armed_ms;
+    bool using_table = true;
+
+    // lookup SoC in table, returning SoC as a percentage
+    float lookup_SoC_table(float voltage);
+};
