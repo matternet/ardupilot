05a7ecd5f37d521355082ca760960bc508ee8554
Andrew Tridgell <andrew@tridgell.net>
2019-10-05 14:01:18 +1000

    AP_UAVCAN: pass batt model to AP_BattMonitor

--- libraries/AP_UAVCAN/AP_UAVCAN.h
+++ libraries/AP_UAVCAN/AP_UAVCAN.h
@@ -118,6 +118,8 @@ public:
         float remaining_capacity_wh;
         float full_charge_capacity_wh;
         uint8_t status_flags;
+        uint32_t model_instance_id;
+        uint8_t model_name[32+1];
     };
 
     uint8_t register_BM_bi_listener_to_id(AP_BattMonitor_Backend* new_listener, uint8_t id);
