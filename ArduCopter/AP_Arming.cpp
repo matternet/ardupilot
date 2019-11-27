4b000fe2758145989eeab9384119b6dc8a0bd701
Andrew Tridgell <andrew@tridgell.net>
2019-11-28 10:17:12 +1100

    Copter: add clearer message on takeoff attempt while disarmed

--- ArduCopter/AP_Arming.cpp
+++ ArduCopter/AP_Arming.cpp
@@ -705,6 +705,10 @@ void AP_Arming_Copter::set_pre_arm_check(bool b)
  */
 bool AP_Arming_Copter::pre_takeoff_checks(void)
 {
+    if (!hal.util->get_soft_armed()) {
+        gcs().send_text(MAV_SEVERITY_CRITICAL,"Takeoff: not armed");
+        return false;
+    }
     float pos_change, alt_change;
     if (!copter.gps.get_pre_arm_pos_change(pos_change, alt_change)) {
         gcs().send_text(MAV_SEVERITY_CRITICAL,"Takeoff: no GPS data");
