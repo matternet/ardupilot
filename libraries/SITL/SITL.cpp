761e1e0c1d5680b410095f66110a8d5bf5444fb9
Andrew Tridgell <andrew@tridgell.net>
2019-11-27 14:41:10 +1100

    SITL: added SIM_MAG_SCALING for testing scale factor errors
    # Conflicts:
    #	libraries/SITL/SITL.h

--- libraries/SITL/SITL.cpp
+++ libraries/SITL/SITL.cpp
@@ -158,6 +158,8 @@ const AP_Param::GroupInfo SITL::var_info2[] = {
 
     AP_GROUPINFO("GPS_DRIFT_S", 35, SITL,  gps_drift_spd, 0),
 
+    AP_GROUPINFO("MAG_SCALING",    60, SITL,  mag_scaling, 1),
+
     AP_GROUPEND
 };
     
