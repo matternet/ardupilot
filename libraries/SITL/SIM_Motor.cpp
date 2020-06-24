7f0b33a9f17abd5e24d84adc6d9afc5c4a9807e5
Andrew Tridgell <andrew@tridgell.net>
2020-06-25 08:36:32 +1000

    SITL: tweaks to bring simulated vehicle closer to matternet vehicle

--- libraries/SITL/SIM_Motor.cpp
+++ libraries/SITL/SIM_Motor.cpp
@@ -117,11 +117,11 @@ void Motor::current_and_voltage(const struct sitl_input &input, float &voltage, 
     // get motor speed from 0 to 1
     float motor_speed = constrain_float((input.servos[motor_offset+servo]-1100)/900.0, 0, 1);
 
-    // assume 10A per motor at full speed
-    current = 10 * motor_speed;
+    // assume 19A per motor at full speed
+    current = 19 * motor_speed;
 
-    // assume 3S, and full throttle drops voltage by 0.7V
+    // assume 12S, and full throttle drops voltage by 12.0V
     if (AP::sitl()) {
-        voltage = AP::sitl()->batt_voltage - motor_speed * 0.7;
+        voltage = AP::sitl()->batt_voltage - motor_speed * 12.0;
     }
 }
--- libraries/SITL/SITL.cpp
+++ libraries/SITL/SITL.cpp
@@ -51,7 +51,7 @@ const AP_Param::GroupInfo SITL::var_info[] = {
     AP_GROUPINFO("SERVO_SPEED",   16, SITL,  servo_speed,  0.14),
     AP_GROUPINFO("GPS_GLITCH",    17, SITL,  gps_glitch,  0),
     AP_GROUPINFO("GPS_HZ",        18, SITL,  gps_hertz,  5),
-    AP_GROUPINFO("BATT_VOLTAGE",  19, SITL,  batt_voltage,  12.6f),
+    AP_GROUPINFO("BATT_VOLTAGE",  19, SITL,  batt_voltage,  48.0f),
     AP_GROUPINFO("ARSPD_RND",     20, SITL,  arspd_noise,  0.5f),
     AP_GROUPINFO("ACCEL_FAIL",    21, SITL,  accel_fail,  0),
     AP_GROUPINFO("BARO_DRIFT",    22, SITL,  baro_drift,  0),
