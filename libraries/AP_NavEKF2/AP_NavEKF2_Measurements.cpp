b737f18eb3dd9279ff778c6c2f0e6fb2e8417636
Akshay Bajaj <akshay@matternet.us>
2020-10-20 16:09:59 -0700

    AP_NavEKF2: Changed the compass-switch message severity from INFO to ALERT

--- libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
+++ libraries/AP_NavEKF2/AP_NavEKF2_Measurements.cpp
@@ -206,7 +206,7 @@ void NavEKF2_core::tryChangeCompass(void)
         // if the magnetometer is allowed to be used for yaw and has a different index, we start using it
         if (compass->healthy(tempIndex) && compass->use_for_yaw(tempIndex) && tempIndex != magSelectIndex) {
             magSelectIndex = tempIndex;
-            gcs().send_text(MAV_SEVERITY_INFO, "EKF2 IMU%u switching to compass %u",(unsigned)imu_index,magSelectIndex);
+            gcs().send_text(MAV_SEVERITY_ALERT, "EKF2 IMU%u switching to compass %u",(unsigned)imu_index,magSelectIndex);
             // reset the timeout flag and timer
             magTimeout = false;
             lastHealthyMagTime_ms = imuSampleTime_ms;
