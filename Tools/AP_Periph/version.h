df5e61342b60076d842e5e6977c1cf250ffbac83
Andrew Tridgell <andrew@tridgell.net>
2020-12-19 11:43:47 +1100

    AP_Periph: prepare for 1.2beta2

--- Tools/AP_Periph/version.h
+++ Tools/AP_Periph/version.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define THISFIRMWARE "AP_Periph V1.2beta1"
+#define THISFIRMWARE "AP_Periph V1.2beta2"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,2,0,FIRMWARE_VERSION_TYPE_BETA
+#define FIRMWARE_VERSION 1,2,0,FIRMWARE_VERSION_TYPE_BETA+1
 
 #define FW_MAJOR 1
 #define FW_MINOR 2
