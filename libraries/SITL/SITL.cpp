42c22508e38d8796afcb8a9526ddb467ded55b85
Brian Hilnbrand <brian.hilnbrand@matternet.us>
2021-03-09 19:51:54 -0800

    MATE-1535 Fix: move param to second var list

--- libraries/SITL/SITL.cpp
+++ libraries/SITL/SITL.cpp
@@ -96,7 +96,6 @@ const AP_Param::GroupInfo SITL::var_info[] = {
     AP_GROUPINFO("GPS2_TYPE",     61, SITL,  gps2_type,  SITL::GPS_TYPE_UBLOX),
     AP_GROUPINFO("ODOM_ENABLE",   62, SITL,  odom_enable, 0),
     AP_SUBGROUPEXTENSION("",      63, SITL,  var_info2),
-    AP_GROUPINFO("EVENT_CMD",     64, SITL,  event_command, SITL::EVENT_ALTERNATE_LANDING),
     AP_GROUPEND
 };
 
@@ -162,6 +161,7 @@ const AP_Param::GroupInfo SITL::var_info2[] = {
     AP_GROUPINFO("MAG_SCALING",    60, SITL,  mag_scaling, 1),
     AP_GROUPINFO("MAG_FAIL_MSK",  61, SITL,  mag_fail_mask, 0),
 
+    AP_GROUPINFO("EVENT_CMD",     62, SITL,  event_command, SITL::EVENT_ALTERNATE_LANDING),
     AP_GROUPEND
 };
     
