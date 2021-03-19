eec44a155ba78615c24b7ab6adf799bcbfce1374
Brian Hilnbrand <brian.hilnbrand@matternet.us>
2021-03-19 13:58:10 -0700

    MATE-1561 Update: Copter, make PARACHUTE msg a DEV_OPTION bit

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -153,6 +153,7 @@ enum PayloadPlaceStateType {
 enum DevOptions {
     DevOptionADSBMAVLink = 1,
     DevOptionVFR_HUDRelativeAlt = 2,
+    DevOption_ParachuteMsg = 4,
 };
 
 //  Logging parameters
