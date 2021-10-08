d111db466e272bac0c95bcdac578f6bb8f80c6de
Brian Hilnbrand <brian.hilnbrand@matternet.us>
2021-10-08 09:27:25 -0700

    MATE-1945 Add: accept mission pause if already paused

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -713,11 +713,15 @@ MAV_RESULT GCS_MAVLINK_Copter::handle_command_long_packet(const mavlink_command_
             }
             return MAV_RESULT_ACCEPTED;
         } else if (is_zero(packet.param2) &&
-                   packet.param1 < 1.9 &&
-                   !copter.wp_nav->get_pause()) {
-            // pause mission
-            gcs().send_text(MAV_SEVERITY_INFO, "Auto: Mission PAUSED");
-            copter.wp_nav->set_pause(true);
+                   packet.param1 < 1.9) {
+            if (!copter.wp_nav->get_pause()) {
+                // pause mission
+                gcs().send_text(MAV_SEVERITY_INFO, "Auto: Mission PAUSED");
+                copter.wp_nav->set_pause(true);
+            }
+            else {
+                gcs().send_text(MAV_SEVERITY_INFO, "Auto: Mission already PAUSED");
+            }
             return MAV_RESULT_ACCEPTED;
         }
 
