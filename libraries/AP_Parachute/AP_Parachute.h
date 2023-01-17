0ecec4a4d09492af426a3c84abc5624d18c49f83
Brian Hilnbrand <brian.hilnbrand@matternet.us>
2023-01-17 11:14:17 -0500

    AS-183 Update: merge OpenDroneID implementation from Ardupilot Copter-4.0-ODID

--- libraries/AP_Parachute/AP_Parachute.h
+++ libraries/AP_Parachute/AP_Parachute.h
@@ -24,13 +24,6 @@
 
 #define AP_PARACHUTE_CRITICAL_SINK_DEFAULT      0    // default critical sink speed in m/s to trigger emergency parachute
 
-#ifndef HAL_PARACHUTE_ENABLED
-// default to parachute enabled to match previous configs
-#define HAL_PARACHUTE_ENABLED 1
-#endif
-
-#if HAL_PARACHUTE_ENABLED
-
 /// @class	AP_Parachute
 /// @brief	Class managing the release of a parachute
 class AP_Parachute {
@@ -139,5 +132,3 @@ private:
 namespace AP {
     AP_Parachute *parachute();
 };
-
-#endif // HAL_PARACHUTE_ENABLED
