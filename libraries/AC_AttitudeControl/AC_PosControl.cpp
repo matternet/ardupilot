c6b9309e8aba20c06a8bec15eff847b7bf464cc3
Adrienne Tiña <adrienne@matternet.us>
2024-11-04 17:37:51 -0800

    Test: Add assert false in get stopping point z

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -554,8 +554,8 @@ void AC_PosControl::get_stopping_point_z(Vector3f &stopping_point) const {
   // stopping_point.z = constrain_float(stopping_point.z, curr_pos_z -
   // POSCONTROL_STOPPING_DIST_DOWN_MAX, curr_pos_z +
   // POSCONTROL_STOPPING_DIST_UP_MAX);
-  stopping_point.z = 0;
-  return;
+  //   stopping_point.z = 0;
+  assert(false);
 }
 
 /// init_takeoff - initialises target altitude if we are taking off
