08165f980271e63d706ff358acea1930cba1e56b
Adrienne Tiña <adrienne@matternet.us>
2025-09-15 14:17:40 -0700

    Fix: add variable name for motor kill motor number, replace magic number

--- ArduCopter/motors.cpp
+++ ArduCopter/motors.cpp
@@ -4,6 +4,7 @@
 #define DISARM_DELAY            20  // called at 10hz so 2 seconds
 #define AUTO_TRIM_DELAY         100 // called at 10hz so 10 seconds
 #define LOST_VEHICLE_DELAY      10  // called at 10hz so 1 second
+#define UINT_8BIT_MAX 255       255
 
 static uint32_t auto_disarm_begin;
 
@@ -193,7 +194,7 @@ void Copter::motors_output()
 
 void set_motor_kill(uint8_t motor_num, bool kill)
 {
-    if (motor_num == 0xFF) {
+    if (motor_num == UINT_8BIT_MAX) {
         motor_kill_all = kill;
     } else if (motor_num < MOTOR_COUNT) {
         motor_kill[motor_num] = kill;
